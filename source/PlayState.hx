7be7d71b910380b72847a371879949dde5a1cd2d
Phlox <168133165+yophlox@users.noreply.github.com>
2024-08-01 06:57:50 -0400

    support for running multiple scripts lol

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,53 +4,81 @@ import flixel.text.FlxText;
 import flixel.FlxState;
 import Script;
 import flixel.FlxG;
+import sys.FileSystem;
+import sys.io.File;
+import sys.FileSystem;
+using StringTools;
 
 class PlayState extends FlxState
 {
     var script:Script;
-    var scriptPath:String = 'assets/data/script.hx';
+    var scripts:Array<Script>;
 
     override public function create()
     {
-        script = new Script();
+        scripts = [];
+        var scriptDir:String = 'assets/data';
 
-        if (sys.FileSystem.exists(scriptPath)) {
-            try {
-                var scriptContent = sys.io.File.getContent(scriptPath);
-                script.loadScript(scriptContent);
-                trace("SCRIPT FOUND AND RUNNING LOL!");
-            } catch (e:Dynamic) {
-                trace("Error reading script: " + e);
-                showNoScriptText();
+        if (FileSystem.exists(scriptDir) && FileSystem.isDirectory(scriptDir)) {
+            var files = FileSystem.readDirectory(scriptDir);
+            for (file in files) {
+                if (file.endsWith(".hx")) {
+                    var scriptPath = scriptDir + "/" + file;
+                    try {
+                        var scriptContent = File.getContent(scriptPath);
+                        var newScript = new Script();
+                        newScript.loadScript(scriptContent);
+                        scripts.push(newScript);
+                        trace("SCRIPT " + file + " FOUND AND RUNNING LOL!");
+                    } catch (e:Dynamic) {
+                        trace("Error reading script " + file + ": " + e);
+                        showNoScriptText(file);
+                    }
+                }
             }
         } else {
-            showNoScriptText();
+            showNoScriptText(null);
         }
 
-        script.interp.variables.set("add", function(value:Dynamic)
-        {
-            add(value);
-        });
+        for (script in scripts) {
+            script.interp.variables.set("add", function(value:Dynamic)
+            {
+                add(value);
+            });
+            script.call("onCreate");
+        }
 
-        script.call("onCreate");
-		super.create();
-        script.call("createPost");
+        super.create();
+
+        for (script in scripts) {
+            script.call("createPost");
+        }
     }
 
     override public function update(elapsed:Float)
     {
-        script.call("update", [elapsed]);
-		if (FlxG.keys.justPressed.R)
-			FlxG.switchState(new PlayState());
-        super.update(elapsed);
-		script.call("updatePost", [elapsed]);
+        for (script in scripts) {
+            script.call("update", [elapsed]);
+        }
 
+        if (FlxG.keys.justPressed.R)
+            FlxG.switchState(new PlayState());
+
+        super.update(elapsed);
+
+        for (script in scripts) {
+            script.call("updatePost", [elapsed]);
+        }
     }
 
-    function showNoScriptText() {
-        var text = new FlxText(0, 0, 0, "No HScript file found, or it's not formatted properly!", 64);
+    function showNoScriptText(fileName:String) {
+        var textMessage = "No HScript file found, or it's not formatted properly!";
+        if (fileName != null) {
+            textMessage = "Error reading script " + fileName + ", or it's not formatted properly!";
+        }
+        var text = new FlxText(0, 0, 0, textMessage, 64);
         text.screenCenter();
         add(text);
-        trace("no script found in the data folder!");
+        trace(textMessage);
     }
 }
